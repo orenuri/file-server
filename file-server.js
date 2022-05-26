77e9876877aeedf94dd5af37d31bc2419b5406fe
Oren Uri <oren.uri@audiocodes.com>
2022-05-26 10:58:29 +0300

    add delete support

--- file-server.js
+++ file-server.js
@@ -6,6 +6,7 @@ const serveIndex = require('serve-index')
 
 const app = express()
 const port = process.env.PORT || 8060
+const basePath='./upload'
 
 const updateFileName = (filename, filePath) => {
     parsedFile = path.parse(filename)
@@ -20,10 +21,10 @@ const updateFileName = (filename, filePath) => {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './upload')
+        cb(null, basePath)
     },
     filename: function (req, file, cb) {
-        const uploadedFileName = updateFileName(file.originalname, './upload')
+        const uploadedFileName = updateFileName(file.originalname, basePath)
         req.uploadedFileName = uploadedFileName
         cb(null, uploadedFileName)
     }
@@ -37,10 +38,10 @@ app.post('/upload', uploadFiles.single('upload'), (req, res) => {
 })
 
 app.put('/upload/:_filename', async (req, res, next) => {
-    const uploadedFileName = updateFileName(req.params._filename, './upload')
+    const uploadedFileName = updateFileName(req.params._filename, basePath)
     console.log('PUT file upload/'+req.params._filename+' saved as upload/' + uploadedFileName)
     try {
-        var diskStream = fs.createWriteStream(path.join('./upload', uploadedFileName))
+        var diskStream = fs.createWriteStream(path.join(basePath, uploadedFileName))
         req.pipe(diskStream)
             .on('finish', () => {
                 console.log('finished saving file')
@@ -54,9 +55,52 @@ app.put('/upload/:_filename', async (req, res, next) => {
     }
 })
 
+app.delete('/upload/allFiles', async (req, res, next) => {
+    let count = 0
+    try {
+        let files = fs.readdirSync(basePath);
+        files.forEach(file => {
+            count++
+            const filePath = path.join(basePath, file);
+            console.log('DELETE file:'+filePath)
+            fs.unlink(filePath, (err => {
+                if (err) console.log(err);
+            }));
+        })
+    } catch(e) {
+        console.log(e)
+    }
+    res.send(`Removed ${count} files`)
+})
+
+app.delete('/upload/:_filename', async (req, res, next) => {
+    let filePath = './upload/'
+    filePath += req.params._filename
+    if(!fs.existsSync(filePath)) {
+        return res.status(404).send(`File ${req.params._filename} not found`)
+    }
+    console.log('DELETE file:'+filePath)
+    fs.unlink(filePath, (err => {
+        if (err) console.log(err);
+    }));
+    res.send('Done')
+})
+
+app.get('/upload/listFiles', async (req, res, next) => {
+    let fileList = []
+    try {
+        let files = fs.readdirSync(basePath);
+        files.forEach(file => {
+            fileList.push(file);
+        })
+    } catch(e) {
+        return res.status(404).send(e)
+    }
+    res.send(fileList)
+})
 
 app.get('/upload/:_filename', async (req, res, next) => {
-    const filePath = './upload/' + req.params._filename
+    const filePath = path.join(basePath, req.params._filename)
     if(!fs.existsSync(filePath)) {
         return res.status(404).send()
     }
@@ -64,8 +108,20 @@ app.get('/upload/:_filename', async (req, res, next) => {
     res.download(filePath)
 })
 
-app.use('/upload', express.static('./upload'), serveIndex('./upload', { 'icons': true }))
+app.use('/upload', express.static(basePath), serveIndex(basePath, { 'icons': true , 'view' :'details'}))
 
 app.listen(port, () => {
     console.log("server is running on port:" + port)
 })
+
+function getFilesInDirectory(filePath) {
+    console.log("\nFiles present in directory:");
+    try {
+        let files = fs.readdirSync(filePath);
+        files.forEach(file => {
+            console.log(file);
+        })
+    } catch(e) {
+        console.log(e)
+    }
+  }
